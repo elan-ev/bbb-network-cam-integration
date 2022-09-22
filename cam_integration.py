703dfca320e6b2b803f8cd5602eb7041cecb8826
Arthur Schreiber <arthur.s.19112000@gmail.com>
2022-09-22 15:40:23 +0200

    Rewrite the script so that all the work happens in the python script

--- cam_integration.py
+++ cam_integration.py
@@ -1,3 +1,7 @@
+import asyncio
+from multiprocessing.sharedctypes import Value
+import signal
+import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -9,13 +13,88 @@ from selenium.webdriver.support import expected_conditions
 
 import sys
 import time
-
+import subprocess
+import select
+import os
+import shlex
 
 #set camera name
 CAMERA_NAME = None
-CHECK_AUDIO_DEVICES = False
 driver = None
+RUNNING = True
 
+def create_loopback_device(device_number, camera_name):
+    """
+    Uses the v4l2loopback module to create a virtual camera device
+    Removes previous v4l2loopback modules, since only one can be active at a time
+    """
+    subprocess.run("sudo modprobe -r v4l2loopback", shell=True)
+    time.sleep(1)
+    subprocess.run(f'sudo modprobe v4l2loopback video_nr={device_number} card_label="{camera_name}" exclusive_caps=1', shell=True)
+    time.sleep(1)
+
+def manage_ffmpeg(stream_url, device_number):
+    """
+    Starts the ffmpeg proess to retrieve the rtsp stream
+    Monitors the cpu usage of the ffmpeg process and restarts it if needed
+    """
+    pid = get_video_stream(stream_url, device_number)
+
+    while RUNNING:
+        if not monitor_process(pid, 1.0):
+            print("Restarting ffmpeg!")
+            try:
+                os.kill(pid, signal.SIGKILL)
+            except OSError:
+                pass
+            pid = get_video_stream(stream_url, device_number)
+        time.sleep(1)
+    try:
+        os.kill(pid, signal.SIGKILL)
+    except OSError:
+        pass
+
+def monitor_process(pid, threshold):
+    """
+    Uses pidstat to monitor the current cpu usage of the process
+    If it is under the threshold, False is returned
+    """
+    print("Monitoring ffmpeg!")
+    monitoring_command =  f"pidstat -p {pid} 1 1 | tail -1 | awk '{{print $8}}'"
+    monitoring_result = subprocess.run(monitoring_command, shell=True, capture_output=True, text=True)
+    
+    try:
+        cpu_usage = float(monitoring_result.stdout.replace(",","."))
+    except ValueError:
+        return False
+    print(cpu_usage)
+
+    if cpu_usage < threshold:
+        print(f"There is a problem with process {pid}!")
+        return False
+    
+    return True
+
+
+def get_video_stream(stream_url, device_number):
+    """
+    Uses ffmpeg to retrieve the rtsp stream and play it into the virtual camera device
+    """
+    command = f"ffmpeg -loglevel error -rtsp_transport tcp -i {stream_url} -f v4l2 -vcodec rawvideo -pix_fmt yuv420p /dev/video{device_number}"
+    print(command)
+
+    ffmpeg_proc = subprocess.Popen(shlex.split(command), shell=False)
+
+    print(f"PID: {ffmpeg_proc.pid}")
+
+    return ffmpeg_proc.pid
+    
+def create_virtual_mic(microphone_name):
+    subprocess.run("pactl unload-module module-remap-source", shell=True)
+    subprocess.run("pactl unload-module module-null-sink", shell=True)
+    subprocess.run("pactl load-module module-null-sink sink_name=virtmic sink_properties=device.description=Virtual_Microphone_Sink", shell=True)
+    subprocess.run(f"pactl load-module module-remap-source master=virtmic.monitor source_name=virtmic source_properties=device.description={microphone_name}", shell=True)
+    
 
 def wait_for(element, timeout=10):
     """Waits for an element to be located and clickable."""
@@ -65,13 +144,21 @@ def select_last_option(select_xpath):
 
 
 if __name__ == "__main__":
-
+    #get command line arguments for configuration of the program
     ROOM_URL = sys.argv[1]
     HEADLESS = (sys.argv[2].lower() == "true")
     CAMERA_NAME = sys.argv[3]
-    MIC_NAME = sys.argv[4]
+    RTSP_STREAM = sys.argv[4]
+    MIC_NAME = sys.argv[5]
 
-    print(CAMERA_NAME)
+    #initialize video resources, i.e., the virtual device and the ffmpeg process
+    create_loopback_device(10, CAMERA_NAME)
+    ffmpeg_thread = threading.Thread(target=manage_ffmpeg, args=(RTSP_STREAM, 10))
+    ffmpeg_thread.start()
+
+    #create and initialize audio resources    
+    create_virtual_mic(MIC_NAME)
+
     time.sleep(5)
 
     #get chrome options and add argument for granting camera permission and window maximization
@@ -81,9 +168,8 @@ if __name__ == "__main__":
     if HEADLESS:
         options.add_argument("--headless")
     
-    # options.set_preference("media.navigator.permission.disabled", True)
-    
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
 
     #go to initial website 
     driver.get(ROOM_URL)
@@ -117,48 +203,32 @@ if __name__ == "__main__":
     #activate speakers
     microphone_xpath = '//*[@aria-label="Microphone"]'
     click_button_xpath(microphone_xpath)
-
-
-    time.sleep(10)
-    
-    if CHECK_AUDIO_DEVICES:
-        #expand list for changing audio devices
-        changeAudioDevice_xpath = '//*[@aria-label="Change audio device"]'
-        click_button_xpath(changeAudioDevice_xpath)
-
-        time.sleep(3)
-
-        #take screenshot to see, whether devices are listed. In headless mode in firefox,
-        #devices are not displayed. In normal mode or in chrome, they are.
-        driver.save_screenshot("./Screenshots/microphoneList.png")
-        driver.quit()
-        exit(0)
-
-    shareCamera_xpath = '//*[@aria-label="Share webcam"]'
-    click_button_xpath(shareCamera_xpath)
-
     time.sleep(3)
 
-    #crashes here, if headless and microphone active
-    driver.save_screenshot("./Screenshots/beforeSetCam.png")
+
+    #click the share camera button to open the sharing dialogue
+    shareCamera_xpath = '//*[@aria-label="Share webcam"]'
+    click_button_xpath(shareCamera_xpath)
+    time.sleep(3)
+
+    #select the virtual camera for sharing
     selectCamera_xpath = '//*[@id="setCam"]'
     select_option(selectCamera_xpath, CAMERA_NAME)
-
     time.sleep(2)
 
+    #select video quality for sharing the camera
     selectQuality_xpath = '//*[@id="setQuality"]'
     select_last_option(selectQuality_xpath)
-    time.sleep(2)
+    time.sleep(10)
 
+    #start sharing the camera
     startSharing_xPath = '//*[@aria-label="Start sharing"]'
     click_button_xpath(startSharing_xPath)
-
     time.sleep(1)
 
     #expand list for changing audio devices
     changeAudioDevice_xpath = '//*[@aria-label="Change audio device"]'
     click_button_xpath(changeAudioDevice_xpath)
-
     time.sleep(1)
 
     #choose virtual microphone by its given name
@@ -168,3 +238,5 @@ if __name__ == "__main__":
     time.sleep(100)
 
     driver.quit()
+    
+    RUNNING = False
